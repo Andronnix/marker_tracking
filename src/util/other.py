f1e15ddfcacb860a19b25a0cf2812a862eb55fed
Andronnix <w4UvOQY4LvLa>
2018-01-10 23:26:23 +0300

    utility functions documentation improved

--- src/util/other.py
+++ src/util/other.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
+from typing import Iterator
 from itertools import chain, zip_longest
 
 
 def mult(m1, m2):
-    """Multiplies two affine transforamtion matrces"""
+    """Multiplies two affine transformation matrices of 2x3 size"""
+    assert np.shape(m1) == (2, 3)
+    assert np.shape(m2) == (2, 3)
+
     m1_temp = np.vstack((m1, [0, 0, 1]))
     m2_temp = np.vstack((m2, [0, 0, 1]))
     result = m1_temp * m2_temp
@@ -13,6 +17,7 @@ def mult(m1, m2):
 
 
 def flip_points(a):
+    """[(x, y), ...] -> [(y, x), ...]"""
     a = np.array(a)
     return np.flip(a, 1)
 
@@ -25,16 +30,25 @@ def transform32(points, H, add=(0, 0)):
     return np.int32(cv2.perspectiveTransform(points.reshape(1, -1, 2), H).reshape(-1, 2) + add)
 
 
-def flatmap(func, *iterable):
+def flatmap(func, *iterable) -> Iterator:
+    """Flattens iterable and applies func to each element
+       [[a, b], [c, d]] -> f(a), f(b), f(c), f(d)
+    """
     return map(func, chain(*chain(*iterable)))
 
 
-def flatmap2(func, *iterable):
+def flatmap2(func, *iterable) -> Iterator:
+    """Flattens iterable and applies func to each element
+       [[[a, b]], [[c, d]]] -> f(a), f(b), f(c), f(d)
+    """
     return map(func, chain(*chain(*chain(*iterable))))
 
 
 def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    """
+    Collect data into fixed-length chunks or blocks
+    grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+
+    """
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
